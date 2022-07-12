e29368230af8d640f38dd75f0efbcae3457ed92d
Mark Venema <markavenema@gmail.com>
2022-07-13 08:48:47 +0900

    Added Cart and Modal

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,20 @@
+import Modal from '../UI/Modal';
+import classes from './Cart.module.css';
+
+const Cart = props => {
+    const cartItems = <ul className={classes['cart-items']}>{[{id: 'c1', name: 'sushi', amount: 2, price: 12.99}].map(item =><li key={item.id}>{item.name}</li>)}</ul>;
+    return (
+        <Modal onClose={props.onClose}>
+            {cartItems}
+            <div className={classes.total}>
+                <span>Total Amount</span>
+                <span>35.62</span>
+            </div>
+            <div className={classes.actions}>
+                <button onClick={props.onClose} className={classes['button--alt']}>Close</button>
+            </div>
+        </Modal>
+    );
+};
+
+export default Cart;
