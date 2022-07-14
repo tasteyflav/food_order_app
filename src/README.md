db8a0d58a2a8824e71a810c7e1c3b2f21e1e5c3b
Mark Venema <markavenema@gmail.com>
2022-07-14 09:38:37 +0900

    Added Refs and Forward Refs

--- src/README.md
+++ src/README.md
@@ -1,2 +1,8 @@
-# food order app
+# Food Order App
 The food order app project from React: The complete guide on Udemy
+Examples:
+1) React Portals - see: index.html Modal.js
+2) React Context - see: cart-context.js CartProvider.js App.js
+3) React useReducer - see: Cart Provider.js
+4) Refs and Forward Refs - see: MealItemForm.js Input.js
+5) 
--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,10 +1,32 @@
+import { useRef, useState } from 'react';
+
 import Input from '../../UI/Input';
 import classes from './MealItemForm.module.css';
 
 const MealItemForm = (props) => {
+    const [amountIsValid, setAmountIsValid] = useState(true);
+    const amountInputRef = useRef();
+
+    const submitHandler = event => {
+        event.preventDefault();
+
+        const enteredAmount = amountInputRef.current.value;
+        const enterAmountNumber = +enteredAmount;
+
+        if( enteredAmount.trim().length === 0 || 
+            enterAmountNumber < 1 || 
+            enterAmountNumber > 5
+        ) {
+            setAmountIsValid(false);    
+            return;
+        }
+        props.onAddToCart(enterAmountNumber);
+    };
+
     return (
-        <form className={classes.form}>
+        <form className={classes.form} onSubmit={submitHandler}>
             <Input 
+                ref={amountInputRef}
                 label="Amount"
                 input = {{
                     id: 'amount_' + props.id,
@@ -16,6 +38,7 @@ const MealItemForm = (props) => {
                 }}
             />
             <button>+ Add</button>
+            {!amountIsValid && <p>Please enter a valid amount (1-5)</p>}
         </form>
     );
 };
